d9d9844daf5070249a9a7d5f15f32c1cbc53b547
홍준성 <ghdwnstjd13@gmail.com>
2022-08-12 12:17:50 +0900

    Chapter1 > ch07 > float와 double 예제

---
+++ Chapter1/src/ch07/DoubleTest.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class DoubleTest {
+
+    public static void main(String[] args) {
+
+        double dnum = 3.14; // 모든 실수 리터럴들은 기본적으로 double형에 들어가기 때문에 문제가 없다. (일반적으로 double을 가장 많이 쓴다.)
+        float fnum = 3.14F; // flaot형으로 넣기 위해 실수형을 뜻하는 F를 포함해주어야 한다.
+
+        System.out.println(dnum);
+        System.out.println(fnum);
+    }
+}
