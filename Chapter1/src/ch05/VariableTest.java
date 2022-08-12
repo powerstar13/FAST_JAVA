e9d5246a3a51588a2d5ad07a612fb8cb1b5e4c17
홍준성 <ghdwnstjd13@gmail.com>
2022-08-12 10:55:53 +0900

    Chapter1 > ch05 > 변수 선언과 사용하기 예제

---
+++ Chapter1/src/ch05/VariableTest.java
@@ -0,0 +1,14 @@
+package ch05;
+
+public class VariableTest {
+
+    public static void main(String[] args) {
+
+        int age, count; // 변수 선언
+        age =  10; // 대입연산자로 R value(10)를 L value(age)에 대입을 한다는 뜻이다.
+
+        int level = 10000; // 선언과 동시에 초기화할 수 있다. 초기화: 값을 준다는 뜻이다.
+        System.out.println(age);
+        System.out.println(level);
+    }
+}
