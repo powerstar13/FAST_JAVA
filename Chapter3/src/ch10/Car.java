ff814d50ad0155037bcab55df257f7b53b62599b
홍준성 <ghdwnstjd13@gmail.com>
2022-08-19 14:37:37 +0900

    Chapter3 > ch10 > 추상 클래스를 활용한 템플릿 메서드 패턴

---
+++ Chapter3/src/ch10/Car.java
@@ -0,0 +1,30 @@
+package ch10;
+
+public abstract class Car {
+
+    public abstract void drive();
+    public abstract void stop();
+    public abstract void wiper();
+
+    public void startCar() {
+        System.out.println("시동을 켭니다.");
+    }
+
+    public void turnOff() {
+        System.out.println("시동을 끕니다.");
+    }
+
+    public void washCar() {}
+
+    /**
+     * 변하면 안되는 시나리오(템플릿 메서드)
+     */
+    final public void run() {
+        startCar();
+        drive();
+        wiper();
+        stop();
+        turnOff();
+        washCar();
+    }
+}
