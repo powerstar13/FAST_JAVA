902513c5db059906aefb855907832e81b3abdb83
홍준성 <ghdwnstjd13@gmail.com>
2022-08-21 07:45:22 +0900

    Chapter5 > ch07 > 제네릭 타입을 사용하지 않는 경우의 예

---
+++ Chapter5/src/ch07/Plastic.java
@@ -0,0 +1,9 @@
+package ch07;
+
+public class Plastic {
+
+    @Override
+    public String toString() {
+        return "재료는 Plastic입니다.";
+    }
+}
---
+++ Chapter5/src/ch07/Powder.java
@@ -0,0 +1,9 @@
+package ch07;
+
+public class Powder {
+
+    @Override
+    public String toString() {
+        return "재료는 Powder입니다.";
+    }
+}
