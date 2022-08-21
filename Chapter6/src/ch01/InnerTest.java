6d67b65bcf4c49408ae092126cd03abb9f0cc837
홍준성 <ghdwnstjd13@gmail.com>
2022-08-21 18:20:46 +0900

    Chapter6 > ch01 > 정적 내부 클래스

--- Chapter6/src/ch01/InnerTest.java
+++ Chapter6/src/ch01/InnerTest.java
@@ -24,13 +24,41 @@ class OutClass {
     public void usingClass() {
         inClass.inTest();
     }
+
+    static class InStaticClass {
+
+        int iNum = 100;
+        static int sInNum = 200;
+
+        void inTest() {
+            System.out.println("InStaticClass iNum = " + iNum + "(내부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InStaticClass iNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+
+        static void sTest() {
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InStaticClass iNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+    }
 }
 
 public class InnerTest {
 
     public static void main(String[] args) {
 
+        // 인스턴스 내부 클래스 사용하기
         OutClass outClass = new OutClass();
         outClass.usingClass();
+
+        System.out.println();
+
+        // 정적 내부 클래스 사용하기
+        OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+        sInClass.inTest();
+
+        System.out.println();
+
+        OutClass.InStaticClass.sTest();
     }
 }
