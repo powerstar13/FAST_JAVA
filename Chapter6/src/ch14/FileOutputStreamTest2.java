057270371d21faa51768771b796a97d19732daaa
홍준성 <ghdwnstjd13@gmail.com>
2022-08-22 17:53:08 +0900

    Chapter6 > ch14 > 바이트 단위 입출력 스트림 > OutputStream > FileOutputStream 예제
    1. byte[] 배열에 A-Z 까지 넣고 배열을 한꺼번에 파일에 쓰기

---
+++ Chapter6/src/ch14/FileOutputStreamTest2.java
@@ -0,0 +1,31 @@
+package ch14;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest2 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        FileOutputStream fos = new FileOutputStream("Chapter6/src/ch14/output2.txt");
+
+        try (fos) { //java 9 부터 제공되는 기능: try() 안에 변수만 넣어도 된다.
+
+            byte[] bs = new byte[26];
+
+            byte data = 65;
+
+            for (int i = 0; i < bs.length; i++) {
+                bs[i] = data++;
+            }
+
+            fos.write(bs);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("end");
+    }
+}
