35eb6adb0aab96150ff32d806c78931ffb125111
홍준성 <ghdwnstjd13@gmail.com>
2022-08-22 15:28:26 +0900

    Chapter6 > ch11 > 오류의 로그를 남기기 - java.util.logging.Logger 활용

---
+++ Chapter6/src/ch11/LoggerTest.java
@@ -0,0 +1,10 @@
+package ch11;
+
+public class LoggerTest {
+
+    public static void main(String[] args) {
+
+        MyLogger logger = MyLogger.getLogger();
+        logger.log("error test");
+    }
+}
