4a341955723f0e4ad3ed65fa660f66e38a3fc7bc
홍준성 <ghdwnstjd13@gmail.com>
2022-08-22 19:28:03 +0900

    Chapter6 > ch16 > 여러가지 보조 스트림 클래스들 > DataInputStream과 DataOutputStream

---
+++ Chapter6/src/ch16/DataStreamTest.java
@@ -0,0 +1,37 @@
+package ch16;
+
+import java.io.*;
+
+public class DataStreamTest {
+
+    public static void main(String[] args) {
+
+        try(
+            FileOutputStream fos = new FileOutputStream("Chapter6/src/ch16/data.txt");
+            DataOutputStream dos = new DataOutputStream(fos)
+        ) {
+            dos.writeByte(100);
+            dos.writeChar('A');
+            dos.writeInt(10);
+            dos.writeFloat(3.14f);
+            dos.writeUTF("Test");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+
+        try(
+            FileInputStream fis = new FileInputStream("Chapter6/src/ch16/data.txt");
+            DataInputStream dis = new DataInputStream(fis)
+        ) {
+            System.out.println(dis.readByte());
+            System.out.println(dis.readChar());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readFloat());
+            System.out.println(dis.readUTF());
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
